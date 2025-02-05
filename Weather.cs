e6bfe6b796311ac772a4d3e322f7b2cebf9cf5d0
mstark <Ganon11@users.noreply.github.com>
2025-02-05 16:07:40 -0500

    feat: add weather feature to worfbot (#11)

---
+++ Weather.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+
+namespace Ganon11.Worfbot
+{
+  public static class WeatherUtilities
+  {
+    const string WEATHER_API_CONFIGURATION_KEY = "WEATHER_API_KEY";
+
+    public static async Task<WeatherPrediction> CheckWeather(string zipCode, IConfiguration configuration)
+    {
+      string apiKey = configuration[WEATHER_API_CONFIGURATION_KEY];
+      using (HttpClient client = new())
+      {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json; charset=utf-8"));
+        client.DefaultRequestHeaders.Add("User-Agent", "Worfbot-Discord");
+
+        var json = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&appid={apiKey}&units=imperial");
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherPrediction>(json);
+      }
+    }
+  }
+}
---
+++ WeatherModels/WeatherPrediction.cs
@@ -0,0 +1,8 @@
+public class WeatherPrediction
+{
+  public Weather[] weather { get; set; }
+  
+  public Temperatures main { get; set; }
+
+  public string name { get; set; }
+}
